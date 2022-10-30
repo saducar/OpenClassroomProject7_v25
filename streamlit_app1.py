b1d899737e76000253d0f1041f08e987006f5293
saducar <81718775+saducar@users.noreply.github.com>
2022-10-30 17:48:35 +0530

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -170,28 +170,28 @@ if result_plts:
 
         if is_default:
 
-            st.write("Age Vs Amount Income")
-            fig10 = plt.figure(figsize=(10, 5))
-            # fig_1, ax_1 = plt.subplots()
+                st.write("Age Vs Amount Income")
+                fig10 = plt.figure(figsize=(10, 5))
+                # fig_1, ax_1 = plt.subplots()
 
-            plt.bar(data["Age_cat"], data["AMT_INCOME_TOTAL"], color="blue")
-            plt.hlines(y=client["AMT_INCOME_TOTAL"], xmin=0, xmax="60-70")
-            plt.vlines(x=client["Age_cat"], ymin=0, ymax=client["AMT_INCOME_TOTAL"]+10000)
-            # result.groupby(['Age(years)','AMT_INCOME_TOTAL']).sum().unstack().plot()
-            plt.title("Age Groups vs Average Amount of Income")
-            #plt.savefig("./images/" + "AVG_AGE_AMT_OF_INCOME_BAR" + ".png")
-            plt.show()
-            st.pyplot(fig10)
+                plt.bar(data["Age_cat"], data["AMT_INCOME_TOTAL"], color="blue")
+                plt.hlines(y=client["AMT_INCOME_TOTAL"], xmin=0, xmax="60-70")
+                plt.vlines(x=client["Age_cat"], ymin=0, ymax=client["AMT_INCOME_TOTAL"]+10000)
+                # result.groupby(['Age(years)','AMT_INCOME_TOTAL']).sum().unstack().plot()
+                plt.title("Age Groups vs Average Amount of Income")
+                #plt.savefig("./images/" + "AVG_AGE_AMT_OF_INCOME_BAR" + ".png")
+                plt.show()
+                st.pyplot(fig10)
 
-            st.write("Age vs Total Amount Credit")
-            fig11 = plt.figure(figsize=(10, 5))
-            plt.bar(data["Age_cat"], data["AMT_CREDIT"], color="red")
-            plt.hlines(y=client["AMT_CREDIT"], xmin=0, xmax="60-70")
-            plt.vlines(x=client["Age_cat"], ymin=0, ymax=client["AMT_CREDIT"]+10000)
-            plt.title("Age Groups vs Average Amount of Credit")
-            #plt.savefig("./images/" + "AVG_AGE_AMT_OF_CREDIT" + ".png")
-            plt.show()
-            st.pyplot(fig11)
+                st.write("Age vs Total Amount Credit")
+                fig11 = plt.figure(figsize=(10, 5))
+                plt.bar(data["Age_cat"], data["AMT_CREDIT"], color="red")
+                plt.hlines(y=client["AMT_CREDIT"], xmin=0, xmax="60-70")
+                plt.vlines(x=client["Age_cat"], ymin=0, ymax=client["AMT_CREDIT"]+10000)
+                plt.title("Age Groups vs Average Amount of Credit")
+                #plt.savefig("./images/" + "AVG_AGE_AMT_OF_CREDIT" + ".png")
+                plt.show()
+                st.pyplot(fig11)
 
 ## Lime and Shap plots
 
@@ -284,7 +284,7 @@ if shap_plt:
 
     # explain first sample from test data
     start_time = time.time()
-    explainer = shap.TreeExplainer(lb)
+    explainer = shap.TreeExplainer(model)
     shap_values = explainer.shap_values(subsampled_test_data)
     elapsed_time = time.time() - start_time
 
