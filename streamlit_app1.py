ef1806777bf9cb8168fa3e2fb313e83220cea1ce
saducar <81718775+saducar@users.noreply.github.com>
2022-10-30 22:28:25 +0530

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -224,7 +224,7 @@ if lime_plt:
     limeexplainer = LimeTabularExplainer(X_train, class_names=class_names, feature_names = X_train.columns, discretize_continuous = False)
     data_df = data[data['SK_ID_CURR']==id]
     idx = data_df.index[0]# the rows of the dataset
-    st.write(data_df)
+    st.write(idx)
     exp = limeexplainer.explain_instance(X_test[idx], model.predict_proba, num_features=10, labels=class_names)
     components.html(exp.as_html(), height=800)
 
